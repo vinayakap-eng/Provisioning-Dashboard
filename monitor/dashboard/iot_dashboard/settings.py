62d103f2e797891fa458173bc3f464fededb3a49
Vinayak Anil <vinayakap@am.amrita.edu>
2026-01-30 16:17:08 +0530

    Add STATIC_ROOT for collectstatic on hosting

--- monitor/dashboard/iot_dashboard/settings.py
+++ monitor/dashboard/iot_dashboard/settings.py
@@ -139,6 +139,9 @@ USE_TZ = True
 
 STATIC_URL = 'static/'
 STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static')]
+# Directory where `collectstatic` will gather static files for the webserver.
+# On PythonAnywhere this should be inside your project directory.
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
